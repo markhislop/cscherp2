1e3e8d6d10ab5f39133e9280419adbd987774071
Mark Hislop <mark-hislop@outlook.com>
2021-04-04 15:23:48 +0200

    Bike window now is fully interactive with db (retrieve, change, delete)

--- ViewModels/BikeWindowViewModel.cs
+++ ViewModels/BikeWindowViewModel.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Data.Entity;
+using System.Windows;
 
 namespace BikeRental.ViewModels
 {
@@ -15,19 +16,42 @@ namespace BikeRental.ViewModels
         public Bikes SelectedBike { get; set; }
         public IEnumerable<GenderOptions> GenderComboBox { get; set; }
         public RelayCommand RemoveBikeClick { get; set; }
+        public RelayCommand EditBikeClick { get; set; }
         public BikeRentalDb db  { get; set; } = new BikeRentalDb();
-        public ObservableCollection<Bikes> ListOfBikes { get; set; }
         public BikeWindowViewModel()
         {
             db.Bikes.Load();
             RemoveBikeClick = new RelayCommand(RemoveBike);
+            EditBikeClick = new RelayCommand(EditBike);
             GenderComboBox = Enum.GetValues(typeof(GenderOptions)).Cast<GenderOptions>();
+        }
+
+        public void EditBike(object a)
+        {
+            if (SelectedBike != null)
+            {
+                Bikes dbBike = db.Bikes.Local.First(bike => bike.Id == SelectedBike.Id);
+                PropertyCopy.Copy(dbBike, SelectedBike);
+                db.SaveChanges();
+            }
+            else
+            {
+                MessageBox.Show("Er is geen bike gekozen!!!");
+            }
+
         }
 
         public void RemoveBike(object a)
         {
-            db.Bikes.Local.Remove(SelectedBike);
-            db.SaveChanges();
+            if (SelectedBike != null)
+            {
+                db.Bikes.Local.Remove(SelectedBike);
+                db.SaveChanges();
+            }
+            else
+            {
+                MessageBox.Show("Er is geen bike gekozen!!!");
+            }
         }
     }
 }
