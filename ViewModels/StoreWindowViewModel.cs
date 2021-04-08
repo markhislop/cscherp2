02c0389a9cc8304df6721034dd00ebdef3649571
Mark Hislop <mark-hislop@outlook.com>
2021-04-08 11:36:05 +0200

    Added store window functionality (copied from yessir)

--- ViewModels/StoreWindowViewModel.cs
+++ ViewModels/StoreWindowViewModel.cs
@@ -6,6 +6,9 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.Entity;
+using System.Windows;
+
 
 namespace BikeRental.ViewModels
 {
@@ -13,15 +16,40 @@ namespace BikeRental.ViewModels
     {
         public Stores SelectedStore { get; set; }
         public RelayCommand RemoveStoreClick { get; set; }
-
+        public RelayCommand EditStoreClick { get; set; }
+        public BikeRentalDb db { get; set; } = new BikeRentalDb();
         public StoreWindowViewModel()
         {
+            db.Stores.Load();
+            EditStoreClick = new RelayCommand(EditStore);
             RemoveStoreClick = new RelayCommand(RemoveStore);
         }
 
+        public void EditStore(object a)
+        {
+            if (SelectedStore != null)
+            {
+                Stores dbStore = db.Stores.Local.First(store => store.Id == SelectedStore.Id);
+                PropertyCopy.Copy(dbStore, SelectedStore);
+                db.SaveChanges();
+            }
+            else
+            {
+                MessageBox.Show("Er is geen store gekozen!!!");
+            }
+        }
         public void RemoveStore(object a)
         {
-            // ListOfStores.Remove(SelectedStore);
+            if (SelectedStore != null)
+            {
+                db.Stores.Local.Remove(SelectedStore);
+                db.SaveChanges();
+            }
+            else
+            {
+                MessageBox.Show("Er is geen Store gekozen!!!");
+            }
+
         }
     }
 }
