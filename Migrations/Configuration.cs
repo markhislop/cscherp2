ccc6dbe01f02b4a6c2c59ac6a4842ae8d5a9b5a4
Mark <s1119394@student.windesheim.nl>
2021-04-08 11:37:45 +0200

    stash

--- Migrations/Configuration.cs
+++ Migrations/Configuration.cs
@@ -9,7 +9,7 @@
     {
         public Configuration()
         {
-            AutomaticMigrationsEnabled = true;
+            AutomaticMigrationsEnabled = false;
         }
 
         protected override void Seed(BikeRental.Models.BikeRentalDb context)
